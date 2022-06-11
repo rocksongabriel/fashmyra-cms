91b4e5f257c98538bcd95ccbcf928a804b67f234
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-06-11 18:26:46 +0000

    Add ssl key to connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -12,6 +12,9 @@ module.exports = ({ env }) => {
         database,
         user,
         password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       debug: false,
     },
